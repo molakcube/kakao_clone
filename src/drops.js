c7ef11cf662a7649c66fbf070066db45a85d4eb0
JunHoe Heo <jhjjang0902@gmail.com>
2022-08-05 20:56:53 +0900

    class name 다시 수정

--- src/drops.js
+++ src/drops.js
@@ -3,7 +3,7 @@ const body = document.querySelector("main");
 function close(event){
     const click = event.target;
     const findDad = click.parentNode.parentNode.parentNode.className;
-    const arrow = document.querySelector(`.${findDad} .blocksection`);
+    const arrow = document.querySelector(`.${findDad} .block_section`);
     arrow.classList.toggle("hidden");
     console.log(arrow);
 }
