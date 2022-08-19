78601be82e127f530da22a55a6f2075e5c0b05ca
JunHoe Heo <jhjjang0902@gmail.com>
2022-08-19 17:38:08 +0900

    solve can't targeting

--- src/drops.js
+++ src/drops.js
@@ -1,11 +1,10 @@
-const body = document.querySelector("main");
-
-function close(event){
-    const click = event.target;
-    const findDad = click.parentNode.parentNode.parentNode.className;
-    const arrow = document.querySelector(`.${findDad} .block_section`);
-    arrow.classList.toggle("hidden");
-    console.log(arrow);
+function close (event) {
+    const button = event.target.parentElement.className;
+    const parent = event.target.parentElement.parentElement.parentElement.className;
+    if(button == "button") {
+        const target = document.querySelector(`.${parent} .block_section`);
+        target.classList.toggle("hidden");
+    } 
 }
 
-body.addEventListener("click", close);
+window.addEventListener("click", close)
